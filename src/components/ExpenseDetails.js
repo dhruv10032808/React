9aede87fe2bd3d9a891bd45f72bf9e678ebbe080
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-01-14 14:57:27 +0530

    React 4

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,10 @@
+import './ExpenseItem.css'
+function ExpenseDetails(props){
+     return(<div>
+        {/* <div className="expense.item__description"> */}
+{ <h2>{props.title}</h2> }
+        <div className="expense.item__price">Rs {props.amount}</div>
+       {/*</div>*/}
+     </div>)
+}
+export default ExpenseDetails;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css"
-function ExpenseItem(props){
+function ExpenseItem(props){ 
     return (
     <div className="expense-item">
-       <div>{props.date.toISOString()}</div>
-       <div className="expense.item__description">
-        <h2>{props.title}</h2>
-        <div className="expense.item__price">Rs {props.amount}</div>
-        <div>{props.loc}</div>
-       </div>
+       <ExpenseDate date={props.date}></ExpenseDate>
+       <ExpenseDetails amount={props.amount} title={props.title}></ExpenseDetails>
        </div>
        );
 }
