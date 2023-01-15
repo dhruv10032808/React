a683d9cc338896b94b150af0c11a4fe07c98c678
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-01-15 20:59:27 +0530

    react 8

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from '../UI/Card'
 import "./ExpenseItem.css"
 const ExpenseItem=(props)=>{ 
-   const changeHandler=()=>{
-      console.log("Button clicked!!")
+   const [title,setTitle]=useState(props.title);
+   const clickHandler=()=>{
+      setTitle('Updated!');
+      console.log(title);
+   }
+   const [amount,setAmount]=useState(props.amount);
+   const addHandler=()=>{
+      setAmount(100);
    }
    return( 
     <Card className="expense-item">
        <ExpenseDate date={props.date}></ExpenseDate>
-       <ExpenseDetails amount={props.amount} title={props.title}></ExpenseDetails>
-       <button onClick={changeHandler}>Change Title</button>
+       <ExpenseDetails amount={amount} title={title}></ExpenseDetails>
+       <button onClick={clickHandler}>Change Title</button>
+       <button onClick={addHandler}>Change amount</button>
        <button>Delete Expense</button>
        </Card>
    );     
